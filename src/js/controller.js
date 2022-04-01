622ebabc0b04cc7957c5b976c3036f983d41ee93
bahare nikoosiyar <bahare.nikoosiyar@gmail.com>
2022-04-01 19:48:02 +0430

    draw simple line (start / move function)

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,2 +1,40 @@
-const canvas = document.querySelector('#canvas');
+const canvas = document.getElementById('canvas');
 console.log(canvas);
+
+canvas.width = window.innerWidth;
+canvas.height = 800;
+
+let context = canvas.getContext('2d');
+
+context.fillStyle = "white";
+context.fillRect(0,0,canvas.width,canvas.height);
+
+let drawColor = 'black';
+let drawWidth = '2';
+let isDrawing = false;
+
+const start = function(event) {
+    if (isDrawing) isDrawing = false; else isDrawing=true;
+    context.beginPath();
+    context.moveTo(event.clientX -  canvas.offsetLeft , event.clientY -  canvas.offsetTop);
+    event.preventDefault();
+}
+
+const draw = function(event) {
+        if (isDrawing) {
+            context.lineTo(event.clientX +  canvas.offsetLeft , event.clientY +  canvas.offsetTop);
+            context.strokeStyle = drawColor;
+            context.lineWidth = drawWidth;
+            context.lineCap = "round";
+            context.lineJoin = "round";
+            context.stroke();     
+        }
+}
+
+canvas.addEventListener('touchstart',start,false);
+canvas.addEventListener('touchmove',draw,false);
+canvas.addEventListener('mousedown',start,false);
+canvas.addEventListener('mousemove',draw,false);
+
+
+
