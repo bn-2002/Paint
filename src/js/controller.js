60b06d0582497d80853cfe8861eb5da31ace6195
bahare nikoosiyar <bahare.nikoosiyar@gmail.com>
2022-04-01 22:00:25 +0430

    color picker

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,40 +1,92 @@
-const canvas = document.getElementById('canvas');
+const canvas = document.getElementById("canvas");
 console.log(canvas);
 
 canvas.width = window.innerWidth;
 canvas.height = 800;
 
-let context = canvas.getContext('2d');
+let context = canvas.getContext("2d");
 
 context.fillStyle = "white";
-context.fillRect(0,0,canvas.width,canvas.height);
+context.fillRect(0, 0, canvas.width, canvas.height);
 
-let drawColor = 'black';
-let drawWidth = '2';
+let drawColor = "black";
+let drawWidth = "2";
 let isDrawing = false;
 
-const start = function(event) {
-    if (isDrawing) isDrawing = false; else isDrawing=true;
-    context.beginPath();
-    context.moveTo(event.clientX -  canvas.offsetLeft , event.clientY -  canvas.offsetTop);
-    event.preventDefault();
+const start = function (event) {
+  if (isDrawing) {
+    context.stroke();
+    context.closePath();
+    isDrawing = false;
+  } else {
+    isDrawing = true;
+  }
+  context.beginPath();
+  context.moveTo(
+    event.clientX - canvas.offsetLeft,
+    event.clientY - canvas.offsetTop
+  );
+  event.preventDefault();
+};
+
+const draw = function (event) {
+  if (isDrawing) {
+    context.lineTo(
+      event.clientX + canvas.offsetLeft,
+      event.clientY + canvas.offsetTop
+    );
+    context.strokeStyle = drawColor;
+    context.lineWidth = drawWidth;
+    context.lineCap = "round";
+    context.lineJoin = "round";
+    context.stroke();
+  }
+  event.preventDefault();
+};
+
+canvas.addEventListener("touchstart", start, false);
+canvas.addEventListener("touchmove", draw, false);
+canvas.addEventListener("mousedown", start, false);
+canvas.addEventListener("mousemove", draw, false);
+
+const colorFileds = document.querySelectorAll(".color-filed");
+
+colorFileds.forEach(colorField => {
+  colorField.addEventListener("click", function () {
+    changeLinecolor(colorField.style.backgroundColor);
+  });
+});
+
+const changeLinecolor = function (color) {
+  drawColor = color;
+};
+
+let colorPicker = document.querySelector('.color-picker');
+
+let defaultColor = '#145539';
+
+window.addEventListener("load", startup, false);
+function startup() {
+  colorPicker.value = defaultColor;
+  colorPicker.addEventListener("change", updateAll, false);
+  colorPicker.select();
 }
 
-const draw = function(event) {
-        if (isDrawing) {
-            context.lineTo(event.clientX +  canvas.offsetLeft , event.clientY +  canvas.offsetTop);
-            context.strokeStyle = drawColor;
-            context.lineWidth = drawWidth;
-            context.lineCap = "round";
-            context.lineJoin = "round";
-            context.stroke();     
-        }
+
+const updateAll = function(e) {
+
+  defaultColor = e.target.value;
+
+  changeLinecolor(defaultColor);
+
+
+  colorFileds.forEach((colorField) => {
+    if (colorField.nextElementSibling) {
+      console.log(colorField);
+      colorField.style.backgroundColor = colorField.nextElementSibling.style.backgroundColor;
+      } else {
+        colorField.style.backgroundColor = e.target.value;
+      }
+  });
 }
-
-canvas.addEventListener('touchstart',start,false);
-canvas.addEventListener('touchmove',draw,false);
-canvas.addEventListener('mousedown',start,false);
-canvas.addEventListener('mousemove',draw,false);
-
-
 
